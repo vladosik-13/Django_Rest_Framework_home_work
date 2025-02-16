3a4ce2ae093495206518bfd28420d5f2ac9ef67b
Владислав Кошелев <testvlad59@gmail.com>
2025-02-16 23:33:49 +0300

    для модели курса добавлен сериализатор "поле вывода кол-ва уроков"

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from lms.models import Course, Lesson
 
@@ -7,6 +7,17 @@ class CourseSerializer(ModelSerializer):
     class Meta:
         model = Course
         fields = "__all__"
+
+
+class CourseDetailSerializer(ModelSerializer):
+    count_lessons_in_course = SerializerMethodField()
+
+    def get_count_lessons_in_course(self, lesson):
+        return lesson.object.filter(course=lesson.course).count()
+
+    class Meta:
+        model = Course
+        fields = ("title", "preview", "description", "count_lessons_in_course")
 
 
 class LessonSerializer(ModelSerializer):
--- lms/views.py
+++ lms/views.py
@@ -4,12 +4,16 @@ from rest_framework.generics import (CreateAPIView, DestroyAPIView,
 from rest_framework.viewsets import ModelViewSet
 
 from lms.models import Course, Lesson
-from lms.serializers import CourseSerializer, LessonSerializer
+from lms.serializers import CourseSerializer, LessonSerializer, CourseDetailSerializer
 
 
 class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()
-    serializer_class = CourseSerializer
+
+    def get_serializer_context(self):
+        if self.action == 'retrieve':
+            return CourseDetailSerializer
+        return CourseSerializer
 
 
 class LessonCreateAPIView(CreateAPIView):
@@ -20,6 +24,7 @@ class LessonCreateAPIView(CreateAPIView):
 class LessonListAPIView(ListAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+
 
 
 class LessonRetrieveAPIView(RetrieveAPIView):
