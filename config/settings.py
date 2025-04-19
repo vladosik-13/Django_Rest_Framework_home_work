f0cf6bbd7cc0121537e61afdc82e3945c4d8f459
Владислав Кошелев <testvlad59@gmail.com>
2025-04-19 18:02:07 +0300

    test ci 51

--- config/settings.py
+++ config/settings.py
@@ -21,6 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY environment variable is not set")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
