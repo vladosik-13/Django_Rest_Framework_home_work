87f84f936c7d48019e36b594957b1721c319ff26
Владислав Кошелев <testvlad59@gmail.com>
2025-04-19 14:38:02 +0300

    test ci

--- config/settings.py
+++ config/settings.py
@@ -192,8 +192,9 @@ EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", True)
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "your-email@example.com")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "your-email-password")
 
+
 if "test" in sys.argv:
-    DATABASES ={
+    DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": BASE_DIR / "db.sqlite3",
